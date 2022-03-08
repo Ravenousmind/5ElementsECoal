50b7cc02a03883a9e781d05e0bfdee168bbb58be
Sergio Tavares <sergaotavares@gmail.com>
2022-03-08 09:23:41 +0000

    login

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import {Route, Link, Routes} from "react-router-dom"
 import Home from "./home/Home";
 import Articles from "./articles/Articles";
 import './App.css'
+import Login, {ProtectedRoute} from "./Login/Login";
 
 function App() {
   return (
@@ -9,12 +10,14 @@ function App() {
     <nav>
       <Link to="/">Home</Link>
       <Link to="/articles">News</Link>
+      <Link to="/Login">Login</Link>
     </nav>
 
       <Routes>
         <Route exact={true} path="/" element={<Home/>}/>
         <Route exact={true} path="/articles" element={<Articles/>}/>
         <Route path="*" element={() => <p>Page Not Found</p>} />
+        <Route exact={true} path="/Login" element={<Login/>}/>
       </Routes>
     </>
 );
--- server/data/initdb.js
+++ server/data/initdb.js
@@ -2,7 +2,7 @@
 
 const sqlite3 = require("sqlite3").verbose();
 
-let db = new sqlite3.Database('./data/ecoalDB', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE
+let db = new sqlite3.Database('./data/ecoalDB.db', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE
 , (err) => {
     if (err) {
         console.error(err.message);
